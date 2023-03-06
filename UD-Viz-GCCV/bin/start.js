92e27708bd3ddcfd33f4244284b979d38ac3b602
valentinMachado <valentin.machado.cpe@gmail.com>
2023-03-06 15:12:02 +0100

    script command

--- UD-Viz-GCCV/bin/start.js
+++ UD-Viz-GCCV/bin/start.js
@@ -1,3 +1,4 @@
+const exec = require("child-process-promise").exec;
 const ExpressAppWrapper = require("@ud-viz/node").ExpressAppWrapper;
 
 const app = new ExpressAppWrapper();
@@ -5,3 +6,9 @@ app.start({
   folder: "./",
   port: 8000,
 });
+
+const printExec = function (result) {
+  console.log("stdout: \n", result.stdout);
+  console.log("stderr: \n", result.stderr);
+};
+exec("npm run build").then(printExec);
